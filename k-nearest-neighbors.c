2b766dea1b64b72160c0a6dc400a9b1f70d9c75d
codingflojo <blempt@gmx.net>
2019-10-19 09:43:08 +0200

    input.txt: Example of data for classification k-nearest-neighbors.c: created File added funktionality for reading the file and saving in a list of structs,

---
+++ k-nearest-neighbors.c
@@ -0,0 +1,121 @@
+/* k_nearest_neigbhours */
+/*codingflojo*/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef struct column {
+	double value;
+	struct column *nextColumn;
+} column;
+
+typedef struct line {
+	struct column *firstColumn;
+	struct line *nextLine;
+	char *classification;
+} line;
+
+typedef struct rank {
+	struct line* linePointer;
+	double distance;
+} rank;
+
+void readFile(FILE** file, char** code);
+void createList(line* lineHeader, FILE* file, char *code);
+void freeList(line* lineHeader);
+
+int main() {
+	FILE* file = NULL;
+	char* code = NULL;
+	readFile(&file, &code);
+	line *lineHeader = (line*)malloc(sizeof(line));
+	createList(lineHeader, file, code);
+	freeList(lineHeader);
+	return 0;
+}
+
+void readFile(FILE** file, char** code) {
+	*file = fopen("input.txt", "r");
+	long fSize = 0;
+
+	if (*file == NULL) {
+		return;
+	}
+	fseek(*file, 0, SEEK_END); // Put Cursor to end of File
+	fSize = ftell(*file); // Get the Position of the Cursor in File
+	fseek(*file, 0, SEEK_SET); // Put the Cursor to the beginning of the File 
+	*code = malloc(fSize); // Allocate space for the File
+}
+
+void createList(line* lineHeader, FILE* file, char *code) {
+	size_t n = 0;
+	int c = 0;
+	int isAlpha = 0;
+	line *lineListPointer = lineHeader;
+	column *columnHeader = (column*)malloc(sizeof(column));
+	column *columnptr = columnHeader;
+	lineListPointer->firstColumn = columnHeader;
+	lineListPointer->classification = NULL;
+	// Read Character after Character till End of File
+	while (c = fgetc(file)) {
+		if ((c > 47 && c < 58) || c == 46) {
+			code[n++] = (char)c;
+		}
+		else if (isalpha(c)) {
+			isAlpha = 1;
+			code[n++] = (char)c;
+		}
+		else {
+			code[n++] = '\0';
+			if (isAlpha) {
+				lineListPointer->classification = malloc(n);
+				strncpy(lineListPointer->classification, code, n);
+			}
+			else {
+				columnptr->value = atof(code);
+			}
+			if (c == EOF) {
+				columnptr->nextColumn = NULL;
+				lineListPointer->nextLine = NULL;
+				break;
+			} else if (c == '\n') {
+				if (lineListPointer->classification == NULL) {
+					lineListPointer->classification = malloc(n);
+					strncpy(lineListPointer->classification, code, n);
+					free(columnptr);
+					columnptr = NULL;
+				}
+				else {
+					columnptr->nextColumn = NULL;
+				}
+				line *newLine = (line*)malloc(sizeof(line));
+				lineListPointer->nextLine = newLine;
+				lineListPointer = lineListPointer->nextLine;
+				lineListPointer->classification = NULL;
+				columnptr = (column*)malloc(sizeof(column));
+				lineListPointer->firstColumn = columnptr;
+			} else {
+				columnptr->nextColumn = (column*)malloc(sizeof(column));
+				columnptr = columnptr->nextColumn;
+			}
+			n = 0;
+		}
+	}
+	lineListPointer->nextLine = NULL;
+}
+
+void freeList(line* lineHeader) {
+	while (lineHeader != NULL) {
+		column* currColumn = lineHeader->firstColumn;
+		while (currColumn != NULL) {
+			column* temp = currColumn;
+			currColumn = currColumn->nextColumn;
+			free(temp);
+		}
+		line* currLine = lineHeader;
+		lineHeader = lineHeader->nextLine;
+		free(currLine);
+	}
+}
